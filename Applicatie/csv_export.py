944e0ad02fe10262d429187935aaf10d5d25a209
BogdanV45 <bogdanvincent32@gmail.com>
2023-12-31 04:06:42 +0100

    Wijziging naam

---
+++ Applicatie/csv_export.py
@@ -0,0 +1,29 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('db/soldaten_database.db')
+cursor = conn.cursor()
+
+query = """
+    SELECT s.voornaam, s.familienaam, s.geboortedatum, s.stamnummer, s.rang, s.component, w.serienummer
+    FROM soldaten s
+    INNER JOIN wapens w ON s.wapen_serienummer = w.serienummer
+"""
+
+cursor.execute(query)
+
+data = cursor.fetchall()
+
+csv_file_path = 'exported_data/soldaten_en_wapens.csv'
+
+with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    
+    csv_writer.writerow([i[0] for i in cursor.description])
+    
+    csv_writer.writerows(data)
+
+
+conn.close()
+
+print(f"Gegevens zijn succesvol naar '{csv_file_path}' geschreven.")
--- Test 1/README.md
+++
@@ -1,2 +0,0 @@
-# Opdracht_Python_2324
-Eindopdracht Python 1ste semester SJ 2023-2024.
